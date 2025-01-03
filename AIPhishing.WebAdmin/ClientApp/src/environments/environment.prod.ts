bc99a18c385927b477a0c09f42a80f77da8e5bd0
Okan Kilic <s.okankilic@gmail.com>
2025-01-03 15:49:58 +0300

    final commit

--- AIPhishing.WebAdmin/ClientApp/src/environments/environment.prod.ts
+++ AIPhishing.WebAdmin/ClientApp/src/environments/environment.prod.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  // baseUrl: 'https://localhost:7247'
+  production: true,
+  // baseUrl: 'https://ss.bait.technology:7247'
 };
 
 /*
